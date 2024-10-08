f714ebffb2ebde000600248838bf63b803a3f635
unknown <mohammedfedaily@gmail.com>
2024-10-08 13:43:05 +0200

    feat:UI v3 for the profile

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -7,33 +7,16 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
   imports: [
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
-      useFactory: (configService: ConfigService): TypeOrmModuleOptions => {
-        const isProduction = configService.get('NODE_ENV') === 'production';
-        if (isProduction) {
-          const url = new URL(configService.get('JAWSDB_URL'));
-          return {
-            type: 'mysql',
-            host: url.hostname,
-            port: parseInt(url.port, 10),
-            username: url.username,
-            password: url.password,
-            database: url.pathname.substr(1),
-            entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-            synchronize: false,
-          };
-        } else {
-          return {
-            type: 'mysql',
-            host: configService.get('DB_HOST'),
-            port: +configService.get<number>('DB_PORT'),
-            username: configService.get('DB_USERNAME'),
-            password: configService.get('DB_PASSWORD'),
-            database: configService.get('DB_NAME'),
-            entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-            synchronize: true,
-          };
-        }
-      },
+      useFactory: (configService: ConfigService): TypeOrmModuleOptions => ({
+        type: 'mysql',
+        host: configService.get('DB_HOST'),
+        port: +configService.get<number>('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+        synchronize: configService.get('NODE_ENV') !== 'production',
+      }),
       inject: [ConfigService],
     }),
   ],
