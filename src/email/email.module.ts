1cf55a44baeffece564b9fde93f450ff0b2045fd
unknown <mohammedfedaily@gmail.com>
2024-10-09 08:23:55 +0200

    Update database configuration for Heroku

--- src/email/email.module.ts
+++ src/email/email.module.ts
@@ -1,32 +1,38 @@
 import { Module } from '@nestjs/common';
-import { join } from 'path';
 import { MailerModule } from '@nestjs-modules/mailer';
 import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
 import { ConfigService } from '@nestjs/config';
+import { join } from 'path';
 import { EmailService } from './email.service';
+
 @Module({
   imports: [
     MailerModule.forRootAsync({
-      useFactory: async (config: ConfigService) => ({
-        transport: {
-          host: config.get('MAIL_HOST'),
-          secure: false,
-          auth: {
-            user: config.get('MAIL_USER'),
-            pass: config.get('MAIL_PASSWORD'),
+      useFactory: async (config: ConfigService) => {
+        const isDevelopment = config.get('NODE_ENV') !== 'production';
+        return {
+          transport: {
+            host: config.get('MAIL_HOST'),
+            secure: false,
+            auth: {
+              user: config.get('MAIL_USER'),
+              pass: config.get('MAIL_PASSWORD'),
+            },
           },
-        },
-        defaults: {
-          from: `"No Reply" <${config.get('MAIL_FROM')}>`,
-        },
-        template: {
-          dir: join(process.cwd(), config.get('EMAIL_TEMPLATE_DIR')),
-          adapter: new HandlebarsAdapter(),
-          options: {
-            strict: true,
+          defaults: {
+            from: `"No Reply" <${config.get('MAIL_FROM')}>`,
           },
-        },
-      }),
+          template: {
+            dir: isDevelopment
+              ? join(process.cwd(), config.get('EMAIL_TEMPLATE_DIR'))
+              : join(process.cwd(), 'dist', config.get('EMAIL_TEMPLATE_DIR')),
+            adapter: new HandlebarsAdapter(),
+            options: {
+              strict: true,
+            },
+          },
+        };
+      },
       inject: [ConfigService],
     }),
   ],
