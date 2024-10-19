a0ebd97ac0fa44f8ed314f37935162bf3e9afade
unknown <mohammedfedaily@gmail.com>
2024-10-19 17:46:54 +0200

    update notification controller

--- src/notifications/notifications.controller.ts
+++ src/notifications/notifications.controller.ts
@@ -5,7 +5,6 @@ import {
   Param,
   UseGuards,
   Request,
-  Put,
 } from '@nestjs/common';
 import { NotificationsService } from './notifications.service';
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
@@ -24,7 +23,7 @@ export class NotificationsController {
   async markAsRead(@Request() req, @Param('id') id: string) {
     return this.notificationsService.markAsRead(+id, req.user.userId);
   }
-  @Put('read-all')
+  @Post('read-all')
   async markAllAsRead(@Request() req) {
     return this.notificationsService.markAllAsRead(req.user.userId);
   }
